172041a36fb1dd459785f2968f972ea5129a77f6
Perfectro <launay_g@epitech.eu>
2020-05-12 17:42:25 +0200

    Add Item template + ItemSheet

---
+++ scripts/aos-item-sheet.js
@@ -0,0 +1,29 @@
+export class AoSItemSheet extends ItemSheet {
+
+    /** @override */
+    static get defaultOptions() {
+        console.log(`AoSItemSheet - defaultOptions`);
+        return mergeObject(super.defaultOptions, {
+            classes: ["age-of-sigmar", "sheet", "item"],
+            width: 350,
+            tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}]
+        });
+    }
+
+    /** @override */
+    get template() {
+        let type = this.item.type;
+        return `systems/age-of-sigmar-soulbound/templates/aos-item-sheet-${type}.html`;
+    }
+
+    /** @override */
+    getData() {
+        const data = super.getData();
+        return data;
+    }
+
+    /** @override */
+    activateListeners(html) {
+        super.activateListeners(html);
+    }
+}
--- scripts/hooks.js
+++ scripts/hooks.js
@@ -1,18 +1,25 @@
-import { AoSActor } from "./aos-actor.js";
-import { AoSActorSheet } from "./aos-actor-sheet.js";
+import {AoSActor} from "./aos-actor.js";
+import {AoSActorSheet} from "./aos-actor-sheet.js";
+import {AoSItemSheet} from "./aos-item-sheet.js";
 
-Hooks.once("init", async function() {
+Hooks.once("init", async function () {
     console.log(`Initializing Simple Age of Sigmar : Soulbound`);
-    CONFIG.Combat.initiative = { formula: "1d6", decimals: 2 };
+    CONFIG.Combat.initiative = {formula: "1d6", decimals: 2};
     CONFIG.Actor.entityClass = AoSActor;
     Actors.unregisterSheet("core", ActorSheet);
-    Actors.registerSheet("aos", AoSActorSheet, { types: ["character"], makeDefault: true });
+    Actors.registerSheet("aos", AoSActorSheet, {types: ["character"], makeDefault: true});
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("aos", AoSItemSheet, {
+        types: ["weapon", "armour", "gear", "kharadron", "talent"],
+        makeDefault: true
+    });
     preloadHandlebarsTemplates()
 });
 
 async function preloadHandlebarsTemplates() {
     const templatePaths = [
-        "systems/age-of-sigmar-soulbound/templates/aos-actor-sheet.html"
+        "systems/age-of-sigmar-soulbound/templates/aos-actor-sheet.html",
+        "systems/age-of-sigmar-soulbound/templates/aos-item-sheet.html"
     ];
     return loadTemplates(templatePaths);
 }
